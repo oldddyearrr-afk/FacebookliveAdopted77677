78396303b7d0f4cf12a13dda39883186b637bb8d
oldddyearrr-afk <oldddyearrr@gmail.com>
2025-12-02 00:35:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8476070935:AAHADgmTDVTErkm25hVUt4dWjf6g37sZKEM")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8302564752:AAHBc-S_ezyiywnzuk2yOSIl8l4a9Q-bLc8")
 FFMPEG_CMD = "ffmpeg"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 FACEBOOK_RTMP_URL = "rtmps://live-api-s.facebook.com:443/rtmp/"
